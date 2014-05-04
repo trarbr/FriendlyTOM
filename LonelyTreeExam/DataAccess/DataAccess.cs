d8ffed35c2e2c646a6a9acd93c3a627c40a025ed
Klockz <lasse.bredgaard@gmail.com>
2014-05-04 21:54:54 +0200

    Added CRUD Methods

--- LonelyTreeExam/DataAccess/DataAccess.cs
+++ LonelyTreeExam/DataAccess/DataAccess.cs
@@ -9,19 +9,17 @@ namespace DataAccess
 {
     public class DataAccess
     {
+        #region OpenSQLConnection
         /// <summary>
         /// This method is for connection to the database.
         /// </summary>
         private static void OpenSqlConnection()
         {
             string connectionString = GetConnectionString();
-
             using (SqlConnection connection = new SqlConnection())
             {
                 connection.ConnectionString = connectionString;
-
                 connection.Open();
-
                 Console.WriteLine("State: {0}", connection.State);
                 Console.WriteLine("ConnectionString: {0}",
                     connection.ConnectionString);
@@ -31,11 +29,34 @@ namespace DataAccess
         /// This method is supposed to read the connection string from a text file.
         /// and return it for the open connection method.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>connString</returns>
         static private string GetConnectionString()
         {
             const string connString = @"C:\Users\Spaak\Documents\GitHub\LonelyTreeExam\ConnectString.txt";
             return connString;
         }
+        #endregion
+
+        #region CRUD
+        private static void ReadAll()
+        {
+            
+        }
+
+        private static void Delete()
+        {
+            
+        }
+
+        private static void Insert()
+        {
+            
+        }
+
+        private static void Update()
+        {
+            
+        }
+        #endregion 
     }
 }
