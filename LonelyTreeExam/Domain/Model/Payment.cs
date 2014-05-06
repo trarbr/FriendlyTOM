4f3ab80f4dbde60f2064b8b8705f3b516bfb7778
troxdk <desmaagraa@gmail.com>
2014-05-06 12:43:25 +0200

    added payment and accountability class

---
+++ LonelyTreeExam/Domain/Model/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    class Payment
+    {
+        // backing field & fields
+
+        // Properties
+
+        // Events
+
+        // Constructors
+
+        // Public methods
+
+        // Private methods
+    }
+}
