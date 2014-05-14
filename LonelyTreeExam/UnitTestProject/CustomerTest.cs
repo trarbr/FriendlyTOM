4bc1f65a1240df60031d3d24c42bb6a22a790399
Klockz <lasse.bredgaard@gmail.com>
2014-05-14 21:18:47 +0200

    Test test test
    woop woop

---
+++ LonelyTreeExam/UnitTestProject/CustomerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public ICustomer ValidCustomerInput()
+        {
+            string name = "Christine";
+            string note = "So sweet";
+        }
+    }
+}
