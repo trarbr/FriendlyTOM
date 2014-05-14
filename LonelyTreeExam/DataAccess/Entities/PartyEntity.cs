d06d82559b8bedc3d02801ac059ef61bda3caabd
TBroedsgaard <troels@broedsgaard.com>
2014-05-14 09:31:57 +0200

    add PartyEntity

---
+++ LonelyTreeExam/DataAccess/Entities/PartyEntity.cs
@@ -0,0 +1,36 @@
+﻿using Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    internal class PartyEntity : IParty
+    {
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Note
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
