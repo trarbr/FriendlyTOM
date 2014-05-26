1b14d9803134b8155364a76a79e0ee3db8f957c4
TBroedsgaard <troels@broedsgaard.com>
2014-05-26 12:23:33 +0200

    cleanup Party validation

--- LonelyTreeExam/Domain/Model/AParty.cs
+++ LonelyTreeExam/Domain/Model/AParty.cs
@@ -43,26 +43,9 @@ namespace Domain.Model
         //Validates the name is not empty.
         protected void validateName(string value)
         {
-            string paramName = "Name";
-            validateNullOrWhiteSpace(value, paramName);
-            validateTextLength(value, paramName);
-        }
-
-        //validation method for checking for whitespace or null. 
-        protected void validateNullOrWhiteSpace(string text, string paramName)
-        {
-            if (string.IsNullOrWhiteSpace(text))
+            if (string.IsNullOrWhiteSpace(value))
             {
-                throw new ArgumentOutOfRangeException(paramName, "may not be empty");
-            }
-        }
-
-        //Validation method for checking the length of a string.
-        private void validateTextLength(string text, string paramName)
-        {
-            if (text.Length > 100)
-            {
-                throw new ArgumentOutOfRangeException(paramName, "text may not be over 100 caracters");
+                throw new ArgumentOutOfRangeException("Name", "may not be empty");
             }
         }
         #endregion
