01f53ce08af3f913f6da47339a252ff9cfa17606
Troels Brødsgaard <tr@arbr.dk>
2014-09-15 08:47:43 +0200

    remove unused aboutLabel

--- FriendlyTOM/FriendlyTOM/UserControls/SettingsUserControl.xaml.cs
+++ FriendlyTOM/FriendlyTOM/UserControls/SettingsUserControl.xaml.cs
@@ -39,29 +39,6 @@ namespace FriendlyTOM.UserControls
         public SettingsUserControl()
         {
             InitializeComponent();
-
-            aboutLabel.Text = readAbout();
-        }
-
-        private string readAbout()
-        {
-            // read from README or hardcode?
-            StringBuilder aboutBuilder = new StringBuilder();
-
-            // Version should **not** be hardcoded
-            aboutBuilder.AppendLine("Friendly TOM version 0.1."); 
-            aboutBuilder.AppendLine("Your Friendly Tour Operations Manager.\n");
-            aboutBuilder.Append("Created by Troels Brødsgaard, Peter Ipsen, Mikkel Møller Jensen and Lasse Bredgaard, ");
-            aboutBuilder.AppendLine("in collaboration with Lonely Tree, Ecuador.");
-            // also collaboration with Lonely Tree, ecuadorian tour operator.
-            aboutBuilder.AppendLine("This software is released under the Apache 2.0 license. For full terms, see LICENSE file or web-url.");
-            aboutBuilder.AppendLine("Currently maintained and developed by Troels Brødsgaard.");
-            aboutBuilder.AppendLine("Please send comments, support requests etc to ");
-            aboutBuilder.AppendLine("To see the manual, look at: \n");
-            aboutBuilder.AppendLine("Uses icons from Silk and Ballicons iconsets.");
-
-
-            return aboutBuilder.ToString();
         }
     }
 }
