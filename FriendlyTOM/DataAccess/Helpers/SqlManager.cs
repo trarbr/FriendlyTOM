287c0cae55661a8150dfafa8d309b047b8f7a137
Troels Brødsgaard <tr@arbr.dk>
2014-09-22 11:37:54 +0200

    remove old sql setup scripts

--- FriendlyTOM/DataAccess/Helpers/SqlManager.cs
+++ FriendlyTOM/DataAccess/Helpers/SqlManager.cs
@@ -47,8 +47,7 @@ namespace DataAccess.Helpers
             // try and establish connection
             if (!databaseExists())
             {
-                // if it fails, run setup script
-                //runSetup();
+                // if it fails, restore install backup
                 string currentDirectory = Directory.GetCurrentDirectory();
                 string backupPath = currentDirectory + @"\Helpers\install-FTOM-0_1_0.bak";
                 RestoreDatabase(backupPath);
@@ -143,70 +142,6 @@ namespace DataAccess.Helpers
             {
                 con.Close();
             }
-        }
-
-        private void runSetup()
-        {
-            // create and configure the database
-            using (SqlConnection con = new SqlConnection(serverString))
-            {
-                SqlCommand cmd = con.CreateCommand();
-                cmd.CommandType = System.Data.CommandType.Text;
-
-                string create_command = String.Format("CREATE DATABASE {0}", databaseName);
-
-                cmd.CommandText = create_command;
-
-                con.Open();
-                cmd.ExecuteNonQuery();
-
-                // configure database
-                List<string> commands = getCommands(@"create-settings.sql");
-                foreach (string command in commands)
-                {
-                    cmd = con.CreateCommand();
-                    cmd.CommandText = command;
-                    cmd.ExecuteNonQuery();
-                }
-            }
-
-            // wait to make sure the new database is ready to accept connections
-            System.Threading.Thread.Sleep(10000);
-
-            // create tables and stored procedures
-            using (SqlConnection con = new SqlConnection(serverString + initialCatalog))
-            {
-                SqlCommand cmd = con.CreateCommand();
-                cmd.CommandType = System.Data.CommandType.Text;
-                con.Open();
-
-                List<string> commands = getCommands(@"create-schema.sql");
-
-                foreach (string command in commands)
-                {
-                    cmd = con.CreateCommand();
-                    cmd.CommandText = command;
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        private List<string> getCommands(string filename)
-        {
-            List<string> commands = new List<string>();
-
-            string sql_file = File.ReadAllText(filename);
-            string[] dirt_commands = Regex.Split(sql_file, "^GO", RegexOptions.Multiline);
-            foreach (string dirty_command in dirt_commands)
-            {
-                String command = dirty_command.Trim();
-                if (!String.IsNullOrWhiteSpace(command))
-                {
-                    commands.Add(command);
-                }
-            }
-
-            return commands;
         }
     }
 }
