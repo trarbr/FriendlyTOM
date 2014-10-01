3355ddc0cb8b5d3d8029562f0b5d0d488a155044
Troels Brødsgaard <tr@arbr.dk>
2014-10-01 20:10:44 +0200

    make DAF constructor internal

--- FriendlyTOM/DataAccess/DataAccessFacade.cs
+++ FriendlyTOM/DataAccess/DataAccessFacade.cs
@@ -33,7 +33,7 @@ namespace DataAccess
         /// Initializes a DataAccessFacade for accessing a MS SQL database
         /// </summary>
         /// <param name="test">For integration tests, set test = true to use test database</param>
-        public DataAccessFacade(bool test = false)
+        internal DataAccessFacade(bool test = false)
         {
             /*
             if (!test)
